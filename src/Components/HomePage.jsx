059be39dc9b887f4c7ea03c9c02b219fbdbbe0e0
Gonzalo Martinez <gonchimartinez9@gmail.com>
2025-06-25 17:12:02 -0300

    Realizando primer componente HomePage

---
+++ src/Components/HomePage.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const HomePage = () => {
+  return (
+    <>
+    <section>
+        <div className='container mt-5' >
+            <h1>Home Page</h1>
+            <h2>Tarjeta Bancaria</h2>
+            <p>Welcome to the Home Page</p>
+            <span>aqui deberia de ir una imagen</span>
+            
+        </div>
+
+         <div className='container mt-5' >
+            <h1>Home Page</h1>
+            <h2>Tarjeta Bancaria</h2>
+            <p>Welcome to the Home Page</p>
+            <span>aqui deberia de ir una imagen</span>
+            
+        </div>
+    </section>
+    </>
+  )
+}
+
+export default HomePage
