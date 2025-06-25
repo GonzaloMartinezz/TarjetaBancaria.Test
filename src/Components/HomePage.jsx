2dcd611300b277301bbe62862beb5c17947269f7
Gonzalo Martinez <gonchimartinez9@gmail.com>
2025-06-25 19:57:00 -0300

    creando el Home ,Componente de dos divs con respecto a elejir que tarjeta usa el cliente

--- src/Components/HomePage.jsx
+++ src/Components/HomePage.jsx
@@ -1,18 +1,17 @@
 import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const HomePage = () => {
   return (
-    <>
-    <section>
-        <div className='container mt-5' >
+    <section className='d-flex align-items-center justify-content-between text-light m-0 p-0' style={{width: '50em'}}>
+        <div className='border border-1 border-light p-5' onClick={() => console.log('aqui deberia de ir una imagen')}>
             <h1>Home Page</h1>
             <h2>Tarjeta Bancaria</h2>
             <p>Welcome to the Home Page</p>
             <span>aqui deberia de ir una imagen</span>
-            
         </div>
 
-         <div className='container mt-5' >
+         <div className='border border-1 border-light p-5' >
             <h1>Home Page</h1>
             <h2>Tarjeta Bancaria</h2>
             <p>Welcome to the Home Page</p>
@@ -20,7 +19,6 @@ const HomePage = () => {
             
         </div>
     </section>
-    </>
   )
 }
 
