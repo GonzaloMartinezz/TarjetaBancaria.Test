54256420a0672948d97e31ae664ca6dd77576f57
Gonzalo Martinez <150065620+GonzaloMartinezz@users.noreply.github.com>
2025-07-02 16:29:44 -0300

    creando componente para pagina login

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 import HomePage from './Components/HomePage'
+import LogIn from './Components/LogIn'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -8,6 +9,7 @@ function App() {
   return (
     <>
     <HomePage />
+    <LogIn />
     </>
   )
 }
---
+++ src/Components/LogIn.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const LogIn = () => {
+  return (
+  <>
+  <form>
+    <div className="mb-3">
+      <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
+      
+      <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
+    </div>
+    <div className="mb-3">
+      <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
+     
+    </div>
+    <div className="mb-3 form-check">
+      
+      <label className="form-check-label" for="exampleCheck1">Check me out</label>
+    </div>
+    <button type="submit" className="btn btn-primary">Submit</button>
+  </form>
+</>
+  )
+}
+
+export default LogIn
