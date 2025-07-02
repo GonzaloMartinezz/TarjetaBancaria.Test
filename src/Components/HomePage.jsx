13ae666dddd451b03058138bcafbcc507ab3e4f7
Gonzalo Martinez <150065620+GonzaloMartinezz@users.noreply.github.com>
2025-07-01 21:57:28 -0300

    realizando el home

--- src/Components/HomePage.jsx
+++ src/Components/HomePage.jsx
@@ -1,23 +1,19 @@
 import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import '../Styles/HomePage.css'
 const HomePage = () => {
   return (
-    <section className='d-flex align-items-center justify-content-between text-light m-0 p-0'>
-        <div className='border border-1 border-light p-5' onClick={() => console.log('aqui deberia de ir una imagen')}>
-            <h1>Home Page</h1>
-            <h2>Tarjeta Bancaria</h2>
-            <p>Welcome to the Home Page</p>
-            <span>aqui deberia de ir una imagen</span>
+    <section className='container-botons d-flex flex-column gap-5 '>
+        <div className='contenedor-boton1 border border-1 border-light p-5 w-100'>
+            <h1>Tarjeta de Credito</h1>
         </div>
-
-         <div className='border border-1 border-light p-5' >
-            <h1>Home Page</h1>
-            <h2>Tarjeta Bancaria</h2>
-            <p>Welcome to the Home Page</p>
-            <span>aqui deberia de ir una imagen</span>
-            
+         <div className='contenedor-boton2 border border-1 border-light p-5 w-100' >
+            <h1>Tarjeta de Debito</h1>
         </div>
+           <div className='contenedor-boton3 border border-1 border-light p-5 w-100' >
+            <h1>Caja de Ahorros</h1>
+        </div>
+        
     </section>
   )
 }
