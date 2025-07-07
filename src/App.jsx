df755b98c78243c2a76fb6f2f7fb10509199c122
Gonzalo Martinez <150065620+GonzaloMartinezz@users.noreply.github.com>
2025-07-07 17:26:21 -0300

    estructura de home; inicio ; Login

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
-import { useState } from 'react'
-import './App.css'
-import HomePage from './Components/HomePage'
-import LogIn from './Components/LogIn'
+import React from 'react';
+import HomePage from './Components/HomePage'; // Importa tu componente HomePage
+import './App.css'; // Estilos globales de la aplicación
+import Inicio from './Components/Inicio';
 
 function App() {
-  const [count, setCount] = useState(0)
-
- return (
+  return (
     <div className="App">
+      <Inicio />
       <HomePage />
+      <Login />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
