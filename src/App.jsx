a5491ce5593775743af5d5a2cbb3c39bc5e724b2
Gonzalo Martinez <gonchimartinez9@gmail.com>
2025-07-08 09:35:23 -0300

    creando pages para la estructura del App

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,22 @@
 // src/App.js
 import React from 'react';
-import Navbar from './Components/Navbar';
 import HeroSection from './Components/HeroSection';
 import FeaturesGrid from './Components/FeacturesGrid';
 import Footer from './Components/Footer';
-import './App.css'; // Estilos generales de la App si los tienes
+import './App.css';
 import PromotionsSection from './Components/PromotionsSection';
-import Inicio from './Components/Inicio';
 import HeroStartPage from './pages/HeroStartPage';
+import HomeStartPage from './pages/HomeStartPage';
 
 function App() {
   return (
-    <div className="App-container">
-    <HeroStartPage />
-    <div className="App-Navbar">
-    <Navbar />
-    </div>
-      <main className="main-content"> {/* Un contenedor para el contenido principal */}
-       <HeroSection />  {/* La sección principal con el título y la imagen */}
-       <div className="App-Features"> 
-        <FeaturesGrid /> {/* La cuadrícula de servicios */}
-       </div>
-        {/* Aquí podrías añadir más secciones si tuvieras */}
-        <div className="App-Promotions"></div>
-        <PromotionsSection /> {/* La nueva sección de promociones */}
-      </main>
-      <footer>
-         <Footer /> {/* El pie de página */}
-      </footer>
-      {/* <Footer /> */} {/* El pie de página */}
+    <div className="App">
+      <HeroStartPage />
+      <HomeStartPage />
+      <HeroSection />
+      <FeaturesGrid />
+      <PromotionsSection />
+      <Footer />
     </div>
   );
 }
